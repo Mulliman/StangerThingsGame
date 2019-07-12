486e8327ae00fad7694eaacf0e09c382db5d98d9
Sam Mullins <sammullins.co.uk@gmail.com>
2019-07-12 15:59:18 +0100

    Game works

--- StrangerThings/StrangerThings.Web/Pages/Index.cshtml.cs
+++ StrangerThings/StrangerThings.Web/Pages/Index.cshtml.cs
@@ -9,9 +9,18 @@ namespace StrangerThings.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        public Game.Game Game { get; set; }
+
         public void OnGet()
         {
+            var cache = new Microsoft.Extensions.Caching.Memory.MemoryCache(new Microsoft.Extensions.Caching.Memory.MemoryCacheOptions());
+            var gameFactory = new StrangerThings.Game.GameFactory(new GoogleTranslator.Translator(@"D:\Development\Redweb\StrangerThings\StrangerThings\StrangerThings.Web\strangerthings.json"));
 
+            var gameService = new StrangerThings.Game.GameService(cache, gameFactory);
+
+            var game = gameService.CreateNewGame();
+
+            Game = game;
         }
     }
 }
